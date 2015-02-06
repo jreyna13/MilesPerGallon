77adcc41f852c32766d8664b4d45664babdd5af6
jreyna13 <jreyna13@mail.stmarytx.edu>
2015-02-05 20:03:52 -0600

    Initial MilesPerGallon check in.

---
+++ Assignment2/src/MilesPerGallon.java
@@ -0,0 +1,36 @@
+//Javier Reyna EG1305A 05/05/2015
+
+import java.util.Scanner;
+
+public class MilesPerGallon {
+
+	public static void main(String[] args) {
+		
+		//algorithm used for input of miles
+		
+		double MPG;
+		double miles;
+		double gallons;
+		
+		Scanner keyboard = new Scanner(System.in);
+		
+		System.out.print("Please enter the miles driven: ");
+		
+		miles = keyboard.nextDouble();
+		
+		//algorithm for gallons
+		
+		System.out.print("Please enter the gallons of fuel used: ");
+		
+		gallons = keyboard.nextDouble();
+		
+		//calculate
+		
+		MPG = miles/gallons;
+		
+		
+		System.out.print("The miles per gallon is: " +MPG);
+		
+		System.exit(0);
+	}
+}
